7aac4a8091fafef72b892f32737f584d9f09345c
amumbi924-bit <amumbi924@gmail.com>
2025-09-24 22:47:09 +0300

    Add files via upload

---
+++ surface area of a cylinder .c
@@ -0,0 +1,33 @@
+/*
+Name:Annie Mumbi Kamau 
+Reg no:pa106/G/29214/25
+description :program to compute surface area of a cylinder 
+surface area=2πr²+2πrh
+*/
+
+#include<stdio.h>
+
+int main (){
+
+//variable declaraction
+float pie,radius,height;
+
+//prompt the user to enter pie 
+printf ("enter pie");
+scanf ("%f,&pie");
+
+//prompt the user to enter radius 
+printf ("enter radius");
+scanf("%f,& radius");
+
+//prompt the user to enter the height 
+printf ("enter the height");
+scanf ("%f,&height");
+
+//display surface area of a cylinder 
+printf ("surface area,%2f");
+
+return 0;
+ }
+
+ 
